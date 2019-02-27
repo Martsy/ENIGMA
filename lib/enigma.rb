e28dcc41e28da943c037bb80af349d35c226acf5
Martha Troubh <indexer@icloud.com>
2019-02-27 10:43:54 -0700

    to master

---
+++ lib/enigma.rb
@@ -0,0 +1,50 @@
+require 'date'
+
+class Enigma
+
+  def initialize(message, key, date)
+    @message = message
+    @key = key
+    @date = date
+  end
+
+  def self.random_key
+    Array.new(5) { random_number }.join
+  end
+
+  def self.random_number
+    [0..9].sample
+  end
+
+  def characters
+    ("a".."z").to_a << " "
+  end
+
+  def self.encryption(message)
+    message.chars.each_slice(4).map(&:join)
+  end
+
+  def self.date_offsets(date)
+    (date.to_i ** 2). to_s.chars.last(4).map(&:to_i)
+  end
+
+  def self.key_rotations(key)
+    key.chars.each_cons(2).map(&:join).map(&:to_i)
+  end
+
+  def encrypt(message, key, date)
+    {
+      encryption: message,
+      key: key,
+      date: date
+    }
+  end
+
+  def decrypt(message, key, date)
+    {
+      decryption: message,
+      key: key,
+      date: date
+    }
+  end
+end
---
+++ test/enigma_test.rb
@@ -0,0 +1,37 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/enigma'
+
+class EnigmaTest < Minitest::Test
+
+  def setup
+    @enigma = Enigma.new
+    @enigma.encrypt("hello world", "02715", "040895")
+    @enigma.decrypt("keder ohulw", "02715", "040895")
+  end
+
+  def test_enigma_class_exists
+    assert_instance_of Enigma, @enigma
+  end
+
+  def test_the_key_is_five_numbers
+    random_key = Enigma.random_key
+    assert_equal 5, random_key.length
+  end
+
+  def test_each_key_is_random
+    keys = 1000.times.map { Enigma.random_key }
+    assert_equal enigma.random_key
+  end
+
+  def test_number_is_0_to_9
+    number_shown = 1000.times.map { Enigma.random_key }.join
+    all_numbers = number_shown.uniq.sort.join
+    assert_equal "0123456789", @enigma.random_number
+  end
+
+  def test_encrypt_message_is_printed
+    assert_equal "hello world", @enigma.message
+  end
+
+end
